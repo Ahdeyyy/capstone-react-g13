4a6d0f231f4c09b5d670a653d9322c7da603a846
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2022-04-22 22:25:22 +0100

    Added router functionality

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link} from 'react-router-dom';
+import './styles.css'
+
+
+const Navbar = () => {
+    return (
+        <nav style={{ display: 'flex', margin: '5px', justifyContent:'space-between', }}>
+            <span style={{border: '1px solid red'}}>LOGO</span>
+            <div className="links" style={{ width: '50%', display: 'flex', border: '1px solid red',justifyContent:'space-between'}}>
+                <Link to='/'><p>Home</p></Link>
+                <Link to='/contact'><p>Contact</p></Link>
+                <Link to='/about'><p>About</p></Link>
+                <Link to='/products'><p>Products</p></Link>
+            </div>
+            
+            
+        </nav>
+      );
+}
+ 
+export default Navbar;
---
+++ src/components/Products.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Products = () => {
+    return ( 
+        <p>This is the Products Page</p>
+     );
+}
+ 
+export default Products;
