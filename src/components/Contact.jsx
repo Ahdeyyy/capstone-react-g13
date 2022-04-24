780b64c1781ac879afe612b48a88f6dec7500af2
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2022-04-24 17:49:39 +0100

    Contact page done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import img from '../assets/contact.jpg'
 
 const Contact = () => {
     return ( 
         <div className="contacts">
-            This is the contact page
+            <div className="contactus">
+                <h1>Contact Us</h1>
+            </div>
+            <div className="bottom-container">
+                <div className="bottom1">  
+                    <h2>How to Find Us</h2>
+                    <p>If you have any questions just 
+                        fill in the contact form and we will 
+                        answer you shortly. <br/>
+
+                        Telephone: 08057398547<br/>
+                        E-mail: izakaya@gmail.com<br/>
+                        
+                    </p>
+                    
+                    
+                </div>
+                <div className="bottom2">
+                    <h2>Get in Touch</h2>
+                    <form>
+                        <input placeholder="Name"></input><br/><br/>
+                        <input placeholder="Email"></input>
+                        <br/>
+                        <br/>
+                        <label for='message'>Enter Message:</label>
+                        <textarea  name="message" id="message" cols="20" rows="10"></textarea>
+                    </form>
+                </div>
+            </div>
         </div>
      );
 }
