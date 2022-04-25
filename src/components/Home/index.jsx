5a12952306844ec6a9bc6410dd8e695ff531927d
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-04-25 12:14:58 +0100

    Added the home page component

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Navbar from "../Navbar";
+import About from "./About";
+import Hero from "./Hero";
+import Products from "./Products";
+
+const Home = () => {
+    return ( 
+        <>
+          <header>
+            <Hero />
+          </header>
+          <main>
+            <About />
+            <Products />
+          </main>
+          
+        </>
+     );
+}
+ 
+export default Home;
